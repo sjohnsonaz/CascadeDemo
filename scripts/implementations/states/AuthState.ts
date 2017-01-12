96864caec9f82bd7668068f7f5d17e85ffcb6dc9
Sean Johnson <sjohnson@sjohnsonaz.com>
2017-01-12 14:34:38 -0700

    Fixed login modal.

--- scripts/implementations/states/AuthState.ts
+++ scripts/implementations/states/AuthState.ts
@@ -24,6 +24,7 @@ export default class AuthState extends State {
     }
 
     init() {
+        this.active = false;
         this.loginModel.wrap({
             username: '',
             password: ''
--- scripts/views/login/LoginView.tsx
+++ scripts/views/login/LoginView.tsx
@@ -13,6 +13,15 @@ export default class LoginView extends Component<ILoginViewProps> {
         this.props.authState.login();
     }
 
+    cancel = (event?: Event) => {
+        event.preventDefault();
+        this.props.authState.init();
+    }
+
+    close = () => {
+        this.props.authState.close();
+    }
+
     changeValue(modelProp: string, event?: Event) {
         this.props.authState.loginModel[modelProp] = (event.target as HTMLInputElement).value;
     }
@@ -21,7 +30,7 @@ export default class LoginView extends Component<ILoginViewProps> {
         let {authState} = this.props;
         let {loginModel, active, close} = authState;
         return (
-            <Modal open={active} onclose={close}>
+            <Modal open={active} onclose={this.close} title="Log In">
                 <Form>
                     <FormContainer title="Username">
                         <input type="text" value={loginModel.username} onchange={this.changeValue.bind(this, 'username')} />
@@ -29,7 +38,11 @@ export default class LoginView extends Component<ILoginViewProps> {
                     <FormContainer title="Password">
                         <input type="password" value={loginModel.password} onchange={this.changeValue.bind(this, 'password')} />
                     </FormContainer>
-                    <Button onclick={this.login}>Log In</Button>
+                    <div className="pull-right">
+                        <Button onclick={this.cancel}>Cancel</Button>
+                        &nbsp;
+                        <Button onclick={this.login} theme="primary">Log In</Button>
+                    </div>
                 </Form>
             </Modal>
         );
